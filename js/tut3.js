faebd7f763b52d2fab1bdacea140c0fa268080e6
sawan <sawan@gmail.com>
2019-10-30 00:37:57 +0530

    variables in js

---
+++ js/tut3.js
@@ -0,0 +1,13 @@
+var name = "sawan";
+console.log(name);
+var city = "pune";
+console.log(name,city);
+{
+  let city = "banglore";
+  console.log(city);
+}
+console.log(city);
+
+let arr1 = [10,20,30];
+arr1.pop(25);
+console.log(arr1); 
