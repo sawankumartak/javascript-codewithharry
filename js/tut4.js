def88904e43cb97486b42c3929cf842cb028a36e
sawan <sawan@gmail.com>
2019-10-30 01:02:54 +0530

    Date types in JS

---
+++ js/tut4.js
@@ -0,0 +1,34 @@
+// console.log("hi");
+// Data type in JS
+// 1.String Datatype
+let name = "sawan";
+console.log(typeof name);
+// 2.Number
+let number = 122;
+console.log(typeof number);
+
+// 3.Boolean
+let isDriver = true;
+console.log(typeof isDriver);
+
+// Reference data type
+// 1.Arrays
+let arr1 = [10,20,30,true,"sawan"];
+console.log(typeof arr1);
+// object literals
+let marks = {
+  sawan : 35,
+  rahul : 35,
+  mangesh : 54
+}
+console.log(typeof marks);
+// 3.Function 
+function marksOfStudent()
+  {
+
+  }
+console.log(typeof marksOfStudent);
+
+// Date
+let date  = new Date();
+console.log(typeof date);
