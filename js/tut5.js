8c390a6f8770829cf2a7eea2a5740b6805fe3331
sawan <sawan@gmail.com>
2019-10-30 01:31:27 +0530

    Type conversion tutoril

---
+++ js/tut5.js
@@ -0,0 +1,15 @@
+console.log("Welcome to Type Conversion Tutorial");
+let stri = "5658";
+
+let no = 56;
+console.log(stri);
+console.log(typeof no);
+console.log(typeof stri);
+
+// Number to String
+let no1 =  String(56);
+console.log(typeof no1);
+console.log(no1.toString(),typeof no1);
+// string to float
+let rollNumber = parseFloat("56455");
+console.log(rollNumber.toFixed(5),rollNumber);
