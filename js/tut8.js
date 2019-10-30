495a242bd58f91230806ffb2c33c201241073082
sawan <sawan@gmail.com>
2019-10-30 18:37:33 +0530

    done

---
+++ js/tut8.js
@@ -0,0 +1,15 @@
+// let mygreet = function(name){
+//   console.log(`happy birthday ${name}
+//   wishing another year of joy happiness`);
+// }
+
+// let msg = console.log(mygreet("swan")); 
+
+let obj = {
+  name : "sawan",
+  roll: function(rollno){
+    console.log(`your roll no is ${rollno}`);
+  }
+}
+
+console.log(obj.roll(15));
