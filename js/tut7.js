89d691cb3e148ea4bec85a0029b287cf84a202d4
sawan <sawan@gmail.com>
2019-10-30 14:55:22 +0530

    loops added

--- js/tut7.js
+++ js/tut7.js
@@ -1,12 +1,12 @@
 //Array And Object Tutorials
 
-let arr = [10,20,30];
+// let arr1 = [10,20,30];
+// // console.log(arr);
+// // console.log(arr.length);
+// // console.log(arr.splice(0,1));
+// // console.log(arr.reverse();
+// console.log(arr.push(52));
 // console.log(arr);
-// console.log(arr.length);
-// console.log(arr.splice(0,1));
-// console.log(arr.reverse();
-console.log(arr.push(52));
-console.log(arr);
 
 
 //Objects
@@ -14,4 +14,13 @@ let library = {
   bookname : "Javascript",
   publishDate :  "24-1-1995"
 };
-console.log(library.bookname);
+// console.log(library.bookname);
+for (const key in library) {
+   console.log(`The ${key} is ${library[key]}`);
+}
+
+let arraydemo = [0,20,54,85,78];
+
+arraydemo.forEach(element => {
+  console.log(element);
+});
