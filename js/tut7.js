eba0dcfd825cbe940e45f190fa2d1eb2f7d7dfaf
sawan <sawan@gmail.com>
2019-10-30 14:00:56 +0530

    Array And Objects

---
+++ js/tut7.js
@@ -0,0 +1,17 @@
+//Array And Object Tutorials
+
+let arr = [10,20,30];
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr.splice(0,1));
+// console.log(arr.reverse();
+console.log(arr.push(52));
+console.log(arr);
+
+
+//Objects
+let library = {
+  bookname : "Javascript",
+  publishDate :  "24-1-1995"
+};
+console.log(library.bookname);
