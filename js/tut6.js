ce7733e905c3e3224f59266373156d27cd110994
sawan <sawan@gmail.com>
2019-10-30 13:31:13 +0530

    Tempaltes litrals

---
+++ js/tut6.js
@@ -0,0 +1,16 @@
+//string functions and template litrals
+// let name  = "sawan";
+// let marks = 65;
+// myhtml = `Hello ${name}
+//           You have secured <b> ${marks}  marks
+//           <h4>Congratulations </h4> 
+//           `;
+ 
+// document.body.innerHTML = myhtml;          
+
+let mystring = "this is code with harry tutorial on javascript";
+mystring = mystring.concat( " this is best");
+console.log(mystring);
+console.log(mystring.slice(0,5)); 
+let splited = mystring.split(' ');
+console.log(splited[2]); 
