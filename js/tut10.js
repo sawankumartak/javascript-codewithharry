185df451019d1d3d97435625a69c3aac3fc82c70
sawan <sawan@gmail.com>
2019-10-31 13:40:50 +0530

    new feature added

---
+++ js/tut10.js
@@ -0,0 +1,8 @@
+let a = "javascript";
+let b = document.links
+console.log(b);
+Array.from(b).forEach(function(element,index,text){
+console.log(element);
+let b = document.links;
+
+})
---
+++ js/tut9.js
@@ -0,0 +1,4 @@
+let a = window.clientInformation;
+let b = innerHeight;
+let c = location.reload(http://google.com);
+console.log(b);
